3a420b220c8d86ffe009118e2a242fe3365a5132
lekao2009 <lekao2009@gmail.com>
2022-01-29 21:41:32 -0300

    adicao midleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const app = express()
 app.use(express.json())
 app.use(bodyParserErrorHandler());
 
+
+function logRequisicao(req,res,next)
+{
+    const {url,method} = req;
+    console.log(`Metodo :  ${method} URL : ${url}`)
+    return next()
+}
+
+app.use(logRequisicao)
+
 app.get('/projeto', function (req, res) {
     const { nome } = req.query;
     return res.send(`ola seu nome eh ${nome}`)
